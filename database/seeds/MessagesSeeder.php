f081c890ff44f2701f5535ae545c4ea2ab346402
Xutrurn <kanjoskillz@gmail.com>
2021-04-12 11:59:38 +0200

    creati Seeder Messages e Images

---
+++ database/seeds/MessagesSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Message;
+use App\Apartment;
+
+
+class MessagesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
