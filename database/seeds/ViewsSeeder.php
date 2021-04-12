14bfad9e6b070973f74d76152b5ee3319055d8a8
Xutrurn <kanjoskillz@gmail.com>
2021-04-12 15:56:18 +0200

    Seeder Views fixato

--- database/seeds/ViewsSeeder.php
+++ database/seeds/ViewsSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Database\Seeder;
+//test
+use Faker\Generator as Faker;
+use App\View;
+use App\Apartment;
 
 class ViewsSeeder extends Seeder
 {
@@ -9,8 +13,15 @@ class ViewsSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        //test
+        for ($i=0; $i < 10; $i++) { 
+            $newView = new View();
+            $conteggioViews = Count(Apartment::all()->toArray());
+            $newView->apartment_id = rand(1, $conteggioViews);
+            $newView->date = $faker->dateTime();
+            $newView->save();
+        }
     }
 }
