ec76ba8cb9d98013570a2de10dfa7c550b34e81f
Davide <davidecorsinimilano@gmail.com>
2021-04-12 17:27:56 +0200

    davide single seed 2

---
+++ database/seeds/ImageSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Image;
+use App\Apartment;
+use Faker\Generator as Faker;
+
+class ImageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 0; $i < 10; $i++) {
+            $new_image = new Image();
+        
+            $conteggioImages = Count(Apartment::all()->toArray());
+            $new_image->apartment_id = rand(1, $conteggioImages);
+
+            $new_image->src = $faker->imageUrl(640, 480, 'animals', true);
+            $new_image->img_description = $faker->text(15);
+        
+            $new_image->save();
+        }
+    }
+}
